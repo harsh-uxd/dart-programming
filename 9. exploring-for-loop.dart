c16ed068bd55e6db0ecb8ef7635e1235d06278b9
harsh-netizen <harsh.lashkari99@gmail.com>
2020-11-06 11:40:41 +0530

    daet-programming

---
+++ 9. exploring-for-loop.dart
@@ -0,0 +1,16 @@
+void main() {
+  // for loop - entry control loop
+  for (var i = 1; i <= 10; i++) {
+    if (i % 2 == 0) {
+      print(i);
+    }
+  }
+
+  // for ..in loop
+
+  List planetList = ["Mecury", "Venus", "Earth", "Mars"];
+
+  for (String planet in planetList) {
+    print(planet);
+  }
+}
