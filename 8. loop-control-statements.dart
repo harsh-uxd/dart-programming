3eacffb8d3e8a4d2565ecef7a26ee40782f6930e
harsh-netizen <harsh.lashkari99@gmail.com>
2020-11-06 11:30:45 +0530

    dart-programming

---
+++ 8. loop-control-statements.dart
@@ -0,0 +1,22 @@
+void main() {
+  // loops
+
+  // for loop - definite (entry control)
+  for (var i = 0; i < 5; i++) {
+    print("Hello!");
+  }
+
+  // while loop - indefinite (entry control)
+  var j = 0;
+  while (j < 5) {
+    print("Hello!");
+    j++;
+  }
+
+  // do while loop - indefinite (exit control)
+  var k = 0;
+  do {
+    print("Hello!");
+    k++;
+  } while (k < 5);
+}
